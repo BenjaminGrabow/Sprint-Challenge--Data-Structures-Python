0a653790d648ec7697c8a9f5ca2c5b63e431db51
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-18 09:47:05 +0200

    added solution to ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,22 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
-
+    if None in self.storage:
+      for index, char in enumerate(self.storage):
+        if char == None:
+          self.storage[index] = item
+          break
+    else: 
+      if self.current == 0:
+        self.storage[0] = item
+        self.current = item
+      else:
+        if self.storage.index(self.current) == self.capacity:
+          self.storage[0] = item
+          self.current = item
+        else:
+          self.storage[self.storage.index(self.current) + 1] = item
+          self.current = item
+    
   def get(self):
-    pass
+    return [item for item in self.storage if item != None]
